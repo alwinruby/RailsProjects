0ec5ad3230201589d9cb030161f174d424218fcf
alwinruby <alwinsolanky@gmail.com>
2017-03-20 19:33:05 +0000

    posts controller

--- TumblrApp/tumblr/app/controllers/posts_controller.rb
+++ TumblrApp/tumblr/app/controllers/posts_controller.rb
@@ -8,4 +8,21 @@ class PostsController < ApplicationController
 
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
 end
